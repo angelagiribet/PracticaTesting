bda6ba499e1e265895ce1d5438d0c4bc7ea6cdd3
Àngela Giribet <angelagiribet@gmail.com>
2022-12-26 12:54:12 +0100

    Updates in testing classes from package data

--- test/data/DocPathTest.java
+++ test/data/DocPathTest.java
@@ -1,4 +1,14 @@
 package data;
 
+import javax.print.Doc;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class DocPathTest {
+
+    public void TestingEquals () {
+        DocPath dp1 = new DocPath ("path");
+        DocPath dp2 = new DocPath ("path");
+        assertTrue(dp1.equals(dp2));
+    }
 }
--- test/data/SmallCodeTest.java
+++ test/data/SmallCodeTest.java
@@ -1,4 +1,12 @@
 package data;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class SmallCodeTest {
+
+    public void TestingEquals () {
+        SmallCode sm1 = new SmallCode ("code");
+        SmallCode sm2 = new SmallCode ("code");
+        assertTrue(sm1.equals(sm2));
+    }
 }
