04394119e2f49efe3eaea944f1f794fc57f8d942
Àngela Giribet <angelagiribet@gmail.com>
2022-12-26 12:49:24 +0100

    Updates in DigitalSignatureTest

--- test/data/DigitalSignatureTest.java
+++ test/data/DigitalSignatureTest.java
@@ -2,10 +2,15 @@ package data;
 
 import org.junit.jupiter.api.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class DigitalSignatureTest {
 
     @Test
-    public void TestEquals() {
-        DigitalSignature dS= new DigitalSignature();
+    public void TestingEquals () {
+        DigitalSignature dg1 = new DigitalSignature (byte[] digSign);
+        DigitalSignature dg2 = new DigitalSignature (byte[] digSign);
+        assertTrue(dg1.equals(dg2));
     }
 }
